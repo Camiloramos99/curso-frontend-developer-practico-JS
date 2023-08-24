21886209c3d04365e5f906a04112a10f475c50ce
camilo ramos <camiloramos99@outlook.com.ar>
2023-08-24 09:59:17 -0300

    fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
-
+const menuMobileIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 }
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive");
+}
+
 menuEmail.addEventListener("click", toggleDesktopMenu);
+
+menuMobileIcon.addEventListener("click", toggleMobileMenu);
